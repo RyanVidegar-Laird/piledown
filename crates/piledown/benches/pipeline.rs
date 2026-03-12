45cf1a6483f4d7ac5b5890b48dbda352556f07f6
ryanvl <rvl-git@tuta.io>
2026-03-12 12:20:58 -0400

    bench: add full-pipeline benchmark for single-region BAM processing

---
+++ crates/piledown/benches/pipeline.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use piledown::engine::{runtime, EngineConfig, PileEngine};
+use piledown::region::PileRegion;
+use piledown::types::{LibFragmentType, Strand};
+
+fn bench_full_pipeline(c: &mut Criterion) {
+    let bam_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../../tests/data/SRR21778056-sorted-subsample.bam");
+    let region =
+        PileRegion::new("chr1".into(), 14900, 15200, "bench".into(), Strand::Reverse).unwrap();
+
+    let rt = runtime();
+
+    c.bench_function("full_pipeline_single_region", |b| {
+        b.iter(|| {
+            let config = EngineConfig {
+                bam_path: bam_path.clone(),
+                exclude_flags: None,
+                lib_type: LibFragmentType::Isr,
+                concurrency: 1,
+                index_path: None,
+            };
+            let engine = PileEngine::new(config);
+            rt.block_on(engine.run_collect(vec![region.clone()]))
+                .unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, bench_full_pipeline);
+criterion_main!(benches);
